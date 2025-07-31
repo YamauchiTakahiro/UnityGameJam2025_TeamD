18d5ef2483133cf80191ec2b3cc3c77c1c2ee4dc
YamauchiTakahiro <ca01254022@st.kawahara.ac.jp>
2025-07-31 11:51:29 +0900

    <barrage>script, material,

---
+++ Assets/Script/BarrageScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrageScript : MonoBehaviour
+{
+    [SerializeField] GameObject sphere;
+    private float speed = 300;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject ball = (GameObject)Instantiate(sphere, transform.position, Quaternion.identity);
+            Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
+            ballRigidbody.AddForce(transform.right * speed);
+        }
+    }
+}
