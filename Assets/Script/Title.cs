79430143a284356eaad4ac73467d346e90c2bf66
MitsumuneJuria <ca01254020@st.kawahara.ac.jp>
2025-08-04 14:35:02 +0900

    タイトルシーンからの移行をサンプルからゲームに変更

--- Assets/Script/Title.cs
+++ Assets/Script/Title.cs
@@ -19,6 +19,6 @@ public class Title : MonoBehaviour
 
     public void onClickStartButton()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("InGameScene");
     }
 }
