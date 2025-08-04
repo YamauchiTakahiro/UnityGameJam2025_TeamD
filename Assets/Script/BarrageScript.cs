538b648fb86b7d45df05fb145d33a166899fc939
YamauchiTakahiro <ca01254022@st.kawahara.ac.jp>
2025-08-04 10:50:54 +0900

    <Sound>弾発射時の効果音の追加 (#6)

--- Assets/Script/BarrageScript.cs
+++ Assets/Script/BarrageScript.cs
@@ -8,6 +8,8 @@ public class BarrageScript : MonoBehaviour
     private float timeReset = 1;
     private float time = 0;
     private float speed = 300;
+    public AudioSource audiosource;
+    public AudioClip Laser;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +19,29 @@ public class BarrageScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //一定間隔で実行する
         time += Time.deltaTime;
         if(time > timeReset)
         {
+            //spaceキー長押しで実行する
             if (Input.GetKey("space"))
             {
+                //弾を出す
                 GameObject barrage = (GameObject)Instantiate(sphere, transform.position, Quaternion.identity);
                 Rigidbody barrageRigidbody = barrage.GetComponent<Rigidbody>();
                 barrageRigidbody.AddForce(transform.right * speed);
+                
+                //スペースを押したときに発射音を出す
+                GetComponent<AudioSource>().Play();
                 time = 0;
             }
+            
         }
+        if(Input.GetKey("space"))
+        {
+            audiosource.PlayOneShot(Laser);
+        }
+    
+    
     }
 }
